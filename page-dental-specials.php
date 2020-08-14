d168808c289b027425e19825d69ca1cb5f0bcdda
wilber <parionahuaraca@gmail.com>
2020-08-14 14:30:08 -0500

    cambios

---
+++ page-dental-specials.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Dental Specials
+Template Post Type: post, page, event
+*/ 
+get_header();?>
+ <main class="main py-12 bg-body ">
+    <div class="container">
+    <?php
+    while ( have_posts() ) :
+        the_post(); 
+        the_content(); 
+    endwhile;
+    ?>  
+    </div>
+</main> 
+ 
+ 
+
+ <?php  
+get_footer();
+?>
+
