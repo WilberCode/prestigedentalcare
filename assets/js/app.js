76a190267493267054135eb16184ff9f2537bf1b
wilber <parionahuaraca@gmail.com>
2020-09-09 21:06:36 -0500

    cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,20 @@ jQuery(function ($) {
         menuMobile($)       // Menu Mobile: Show and hide the menu  
         hacksBehaviors($)   // Hacks Behaviors
     });  
+ 
+      $(".target-form-espanol").click(function() {
+        const forEspanol = document.getElementById('form-espanol'); 
+        forEspanol.scrollIntoView({
+          block: 'center',
+        });
+      });
+      $(".target-form-english").click(function() {
+        const forEspanol = document.getElementById('form-english'); 
+        forEspanol.scrollIntoView({
+          block: 'center',
+        });
+      });
+      
    
 });
  
